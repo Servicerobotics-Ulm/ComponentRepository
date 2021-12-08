904be1b4ede716de87615c50997986c01e5b4449
Nayabrasul <thomas.feldmeier@thu.de>
2021-12-08 11:30:40 +0100

    ComponentExerciseRGBDApproachCylinder: added

---
+++ ComponentExerciseRGBDApproachCylinder/smartsoft/src-gen/MixedPortTargetConfigs.cmake
@@ -0,0 +1,19 @@
+# target configurations for ComponentExerciseRGBDApproachCylinderROSExtension
+IF(EXISTS ${ROS_DIR})
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} ${ROS_LIBS})
+ENDIF(EXISTS ${ROS_DIR})
+
+# target configurations for OpcUaBackendComponentGeneratorExtension
+IF(SeRoNetSDK_FOUND)
+# SeRoNetSDK has to be linked at the minimum (in case the component does not have any ports specified for any reason)
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} SeRoNetSDK::SeRoNetSDK)
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} CommBasicObjectsOpcUa)
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} DomainVisionOpcUa)
+ENDIF(SeRoNetSDK_FOUND)
+
+# target configurations for PlainOpcUaComponentExerciseRGBDApproachCylinderExtension
+IF(Open62541CppWrapper_FOUND)
+TARGET_LINK_LIBRARIES(${PROJECT_NAME} Open62541CppWrapper)
+TARGET_COMPILE_DEFINITIONS(${PROJECT_NAME} PUBLIC HAS_OPCUA)
+ENDIF(Open62541CppWrapper_FOUND)
+
